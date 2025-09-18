d9add46bfbc16df72dc941577ff4d0a987d00906
robsu17 <robson.costa134@gmail.com>
2025-09-18 09:17:55 -0300

    feat: environment implemented

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly configService: ConfigService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello() {
+    const port = this.configService.get('PORT');
+    return {
+      message: `Server is running on port ${port}`,
+    };
   }
-}
+}   
