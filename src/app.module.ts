e52081f3421c2caaae4834076e3931d060bfe5ca
robsu17 <robson.costa134@gmail.com>
2025-09-18 09:47:10 -0300

    feat: add Zod for environment validation and update app module configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { CoreModule } from './core/core.module';
+import { validateEnv } from './core/infra/config/env';
 
 @Module({
   controllers: [AppController],
@@ -10,6 +11,7 @@ import { CoreModule } from './core/core.module';
     CoreModule,
     ConfigModule.forRoot({
       isGlobal: true,
+      validate: validateEnv
     }),
   ],
 })
