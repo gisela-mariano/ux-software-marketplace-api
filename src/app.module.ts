84985098915947a2b5878b3a651461a3e47dc505
robsu17 <robson.costa134@gmail.com>
2025-09-18 09:40:05 -0300

    feat: integrate Prisma client and create Core and Auth modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
+import { CoreModule } from './core/core.module';
 
 @Module({
   controllers: [AppController],
   providers: [],
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-  })],
+  imports: [
+    CoreModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/core/infra/database/prisma.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaClient } from "generated/prisma";
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+    constructor() {
+        super({
+            log: ['error', 'warn'],
+            errorFormat: 'pretty'
+        })
+    }
+}
