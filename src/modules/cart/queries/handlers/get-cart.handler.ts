24594b377de0dda0feb829984a26c471400daf80
robsu17 <robson.costa134@gmail.com>
2025-09-22 09:02:24 -0300

    feat: add .env.example and README.md for project setup, update license to MIT in package.json

--- src/modules/cart/queries/handlers/get-cart.handler.ts
+++ src/modules/cart/queries/handlers/get-cart.handler.ts
@@ -2,10 +2,14 @@ import { QueryHandler, IQueryHandler } from "@nestjs/cqrs";
 import { PrismaService } from "src/core/infra/database/prisma.service";
 import { NotFoundException } from "@nestjs/common";
 import { GetCartQuery } from "../get-cart.query";
+import { FileService } from "src/core/infra/multer/file.service";
 
 @QueryHandler(GetCartQuery)
 export class GetCartHandler implements IQueryHandler<GetCartQuery> {
-    constructor(private readonly prismaService: PrismaService) {}
+    constructor(
+        private readonly prismaService: PrismaService, 
+        private readonly fileService: FileService
+    ) {}
 
     async execute(query: GetCartQuery) {
         const { userId } = query;
@@ -27,14 +31,16 @@ export class GetCartHandler implements IQueryHandler<GetCartQuery> {
             throw new NotFoundException('Cart not found');
         }
 
-        // Calcular o preço total e formatar os dados
         let totalPrice = 0;
         const cartItems = cart.products.map(item => {
             const itemTotal = item.product.price * item.quantity;
             totalPrice += itemTotal;
 
             return {
-                product: item.product,
+                product: {
+                    ...item.product,
+                    imageUrl: this.fileService.getFileUrl(item.product.imageUrl)
+                },
                 quantity: item.quantity,
                 itemTotal: itemTotal
             };
